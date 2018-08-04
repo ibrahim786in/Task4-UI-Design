c2091f4a635be8a947b0cc23ef2753eda90f60d8
Mohammad Ibrahim <md.ibrahim786in@gmail.com>
2018-08-05 03:31:20 +0530

    Without Animation first view UI

--- app/src/main/java/com/example/ibrahim/task4ui/MainActivity.java
+++ app/src/main/java/com/example/ibrahim/task4ui/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.ibrahim.task4ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -17,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
         fb.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
