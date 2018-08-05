bb72cca7fc35431775965fe1e685345b75b26fb7
Mohammad Ibrahim <md.ibrahim786in@gmail.com>
2018-08-05 17:15:57 +0530

    UI Project done Without Animation

--- app/src/main/java/com/example/ibrahim/task4ui/MainActivity.java
+++ app/src/main/java/com/example/ibrahim/task4ui/MainActivity.java
@@ -4,20 +4,40 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Button begin, cancel;
+    Spinner sp1, sp2, sp3;
+    LinearLayout L1, L2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        begin = (Button) findViewById(R.id.begin);
+        cancel = (Button) findViewById(R.id.cancel);
+        sp1 = (Spinner) findViewById(R.id.spinner1);
+        sp2 = (Spinner) findViewById(R.id.spinner2);
+        sp3 = (Spinner) findViewById(R.id.spinner3);
+        L1 = (LinearLayout) findViewById(R.id.newAlarm);
+        L2 = (LinearLayout) findViewById(R.id.repleableLayout);
 
-        FloatingActionButton fb = (FloatingActionButton) findViewById(R.id.fb);
-        fb.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton floatBtn = (FloatingActionButton) findViewById(R.id.floatBtn);
+        floatBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+                L1.setVisibility(View.GONE);
+                L2.setVisibility(View.VISIBLE);
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                L1.setVisibility(View.VISIBLE);
+                L2.setVisibility(View.GONE);
             }
         });
     }
